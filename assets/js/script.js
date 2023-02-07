eec0aa5f9374963c1d105e8fdcafefaa90dfb6bf
ProsperityGH <90307071+ProsperityGH@users.noreply.github.com>
2023-02-07 16:57:21 +0100

    Added more features

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const usernameInput = document.querySelector('input[type="text"]');
 const passwordInput = document.querySelector('input[type="password"]');
 const submitButton = document.querySelector('input[type="submit"]');
 const resultContainer = document.createElement("div");
+const revokeButton = document.createElement("button");
+
+window.onload = checkLogin;
 
 resultContainer.style.marginTop = "20px";
 resultContainer.style.fontFamily = "Montserrat", "sans-serif";
@@ -16,6 +19,13 @@ submitButton.addEventListener("click", function (event) {
   if (username === "root" && password === "root") {
     resultContainer.innerHTML = "You are now logged in";
     resultContainer.style.color = "green";
+    localStorage.setItem("loggedIn", "true");
+    usernameInput.disabled = true;
+    passwordInput.disabled = true;
+    submitButton.disabled = true;
+    revokeButton.style.display = "block";
+    const form = submitButton.closest("form");
+    form.appendChild(revokeButton);
   } else {
     resultContainer.innerHTML = "Invalid username or password";
     resultContainer.style.color = "red";
@@ -23,4 +33,41 @@ submitButton.addEventListener("click", function (event) {
 
   const form = submitButton.closest("form");
   form.appendChild(resultContainer);
+});
+
+function checkLogin() {
+  if (localStorage.getItem("loggedIn") === "true") {
+    console.log(resultContainer);
+    resultContainer.innerHTML = "You are already logged in";
+    resultContainer.style.color = "green";
+    revokeButton.style.display = "block";
+    usernameInput.disabled = true;
+    passwordInput.disabled = true;
+    submitButton.disabled = true;
+  } else {
+    resultContainer.innerHTML = "";
+    revokeButton.style.display = "none";
+    usernameInput.disabled = false;
+    passwordInput.disabled = false;
+    submitButton.disabled = false;
+  }
+  const form = submitButton.closest("form");
+  form.appendChild(revokeButton);
+  form.appendChild(resultContainer);
+}
+
+revokeButton.textContent = "Revoke Login";
+revokeButton.style.display = "none";
+revokeButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  checkLogin();
+  localStorage.removeItem("loggedIn");
+  resultContainer.innerHTML = "You have revoked your login";
+  resultContainer.style.color = "red";
+  revokeButton.style.display = "none";
+  usernameInput.disabled = false;
+  passwordInput.disabled = false;
+  submitButton.disabled = false;
+  usernameInput.value = "";
+  passwordInput.value = "";
 });
